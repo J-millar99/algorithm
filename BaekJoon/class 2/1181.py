6d992083b3707a7752d7f3da180363a824d27868
J-millar99 <wlwogus33@gmail.com>
2024-09-22 10:21:06 +0900

    datastructur (stack, queue, deque)
    +
    lambda

---
+++ BaekJoon/class 2/1181.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+data = set()
+for _ in range(n):
+    data.add(input().rstrip())
+
+data = list(data)
+data.sort(key=lambda x: (len(x), x))
+print("\n".join(data))
