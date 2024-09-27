8279490299bb3238447087703ffbc8a54fcaba87
J-millar99 <wlwogus33@gmail.com>
2024-09-28 07:07:28 +0900

    delete for restart

--- 이코테_Python/BinarySearch/정렬된 배열에서 특정 수의 개수 구하기_풀이1.py
+++
@@ -1,8 +0,0 @@
-N, x = map(int, input().split())
-data = list(map(int, input().split()))
-
-result = data.count(x)
-if result == 0:
-    result = -1
-print(result)
-
--- 이코테_Python/DFS&BFS/gcd.py
+++
@@ -1,9 +0,0 @@
-a, b = map(int, input().split())
-
-def get_gcd(a, b):
-    if a % b == 0:
-        return(b)
-    else:
-        return(get_gcd(b, a%b))
-        
-print(get_gcd(a, b))
