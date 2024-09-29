10716958096f2cee302ed9439eee9ca089e7b85a
J-millar99 <wlwogus33@gmail.com>
2024-09-29 18:58:40 +0900

    다이나믹 프로그래밍(누적합)

--- BaekJoon/class 3/1003.py
+++ BaekJoon/class 3/1003.py
@@ -1,21 +1,26 @@
 # 피보나치 함수
 
-def fibonacci(num):
-    if num == 0:
-        count0 += 1
-        return 0
-    elif num == 1:
-        count1 += 1
-        return 1
-    return fibonacci(num - 1) + fibonacci(num - 2)
-    
+def fibonacci(n):
+    # 0과 1의 등장 횟수를 저장할 리스트
+    count_0 = [0] * (n + 1)
+    count_1 = [0] * (n + 1)
 
+    # 초기값 설정
+    count_0[0] = 1  # fib(0) = 0 일 때 0이 1번 등장
+    count_1[1] = 1  # fib(1) = 1 일 때 1이 1번 등장
+
+    # 피보나치 수열과 동시에 등장 횟수 계산
+    for i in range(2, n + 1):
+        count_0[i] = count_0[i - 1] + count_0[i - 2]
+        count_1[i] = count_1[i - 1] + count_1[i - 2]
+    print(count_0.pop(), count_1.pop())
+    
 import sys
 input = sys.stdin.readline
 n = int(input())
 for _ in range(n):
     num = int(input())
-    count0 = 0
-    count1 = 0
-    fibonacci(num)
-    print(count0, count1, sep=" ")
+    if num == 0:
+        print(1, 0)
+    else: 
+        fibonacci(num)
