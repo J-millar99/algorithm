bcbacf16ac688144a62d69914ab8997bfba2e5ca
J-millar99 <wlwogus33@gmail.com>
2024-10-12 20:47:50 +0900

    누적합

--- BaekJoon/class 3/11660.py
+++ BaekJoon/class 3/11660.py
@@ -1,12 +1,22 @@
-# 구간 합 구하기 5
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().rstrip().split())
-_map = [list(map(int, input().rstrip().split())) for _ in range(n)]
+n, m = map(int, input().split())
+_map = [list(map(int, input().split())) for _ in range(n)]
+
+# 2차원 누적합 배열 만들기
+prefixSum = [[0] * (n + 1) for _ in range(n + 1)]
+
+# 누적합 계산
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        prefixSum[i][j] = _map[i - 1][j - 1] + prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1]
+
+# 구간 합 계산
 ret = []
 for _ in range(m):
-    cord = list(map(int, input().rstrip().split()))
-    submap = [row[cord[1] - 1:cord[3]] for row in _map[cord[0] - 1:cord[2]]]
-    ret.append(str(sum(sum(row) for row in submap)))
-print("\n".join(ret))
+    sX, sY, eX, eY = map(int, input().split())
+    result = prefixSum[eX][eY] - prefixSum[sX - 1][eY] - prefixSum[eX][sY - 1] + prefixSum[sX - 1][sY - 1]
+    ret.append(result)
+
+print("\n".join(map(str, ret)))
