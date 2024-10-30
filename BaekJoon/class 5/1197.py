dd5c1f76eb96bd0380e6cd2b1f23fc7c02537b20
J-millar99 <wlwogus33@gmail.com>
2024-10-31 00:29:23 +0900

    그래프 이론

---
+++ BaekJoon/class 5/1197.py
@@ -0,0 +1,44 @@
+# 최소 스패닝 트리
+import sys
+sys.setrecursionlimit(10**6)
+def find_root(parent, x):
+    if parent[x] != x:
+        parent[x] = find_root(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_root(parent, a)
+    b = find_root(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+# 노드의 개수와 간선의 개수 입력받기
+v, e = map(int, input().split())
+parent = [0] * (v + 1)
+
+# 모든 간선을 담을 리스트와 최종 비용을 담을 변수
+edges = []
+result = 0
+
+for i in range(1, v + 1):
+    parent[i] = i
+
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    # 비용순으로 정렬하기 위해서 튜플의 첫 번째 원소를 비용으로 설정
+    edges.append((cost, a, b))
+
+# 간선을 비용순으로 오름차순 정렬
+edges.sort()
+
+# 간선을 하나씩 확인하며
+for edge in edges:
+    cost, a, b = edge
+    # 사이클이 발생하지 않는 경우에만 집합에 포함
+    if find_root(parent, a) != find_root(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+
+print(result)
---
+++ BaekJoon/class 5/1647.py
@@ -0,0 +1,46 @@
+# 도시 분할 계획
+import sys
+input = sys.stdin.readline
+
+def find(parent, x):
+    if parent[x] != x:
+        parent[x] = find(parent, parent[x])
+    return parent[x]
+
+def union(parent, a, b):
+    a = find(parent, a)
+    b = find(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+# 노드의 개수와 간선의 개수 입력받기
+v, e = map(int, input().split())
+parent = [0] * (v + 1)
+
+# 모든 간선을 담을 리스트와 최종 비용을 담을 변수
+edges = []
+result = 0
+
+for i in range(1, v + 1):
+    parent[i] = i
+
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    # 비용순으로 정렬하기 위해서 튜플의 첫 번째 원소를 비용으로 설정
+    edges.append((cost, a, b))
+
+# 간선을 비용순으로 오름차순 정렬
+edges.sort()
+
+# 간선을 하나씩 확인하며
+for edge in edges:
+    cost, a, b = edge
+    # 사이클이 발생하지 않는 경우에만 집합에 포함
+    if find(parent, a) != find(parent, b):
+        union(parent, a, b)
+        result += cost
+        last = cost # 연결이 이어진 경우, cost를 갱신 => 계속 최대값이 됨
+
+print(result - last)
