ba702fe906722fe7fda25a9be032f1372b395504
J-millar99 <wlwogus33@gmail.com>
2024-11-02 15:59:41 +0900

    이중 우선 순위 큐

--- BaekJoon/class 3/7662.py
+++ BaekJoon/class 3/7662.py
@@ -4,45 +4,34 @@ input = sys.stdin.readline
 
 # 최소힙으로 생성
 def _insert(val):
+    if not val in finder:
+        finder[val] = 1
+    else:
+        finder[val] += 1
     heapq.heappush(minq, val) # 최소힙
     heapq.heappush(maxq, -val) # 최대힙
-    finder.add(val)
 
 def _delete(val):
     if val == "1": # 최대값 제거
         while maxq:
             _max = -heapq.heappop(maxq)
-            if _max in finder:
-                finder.remove(_max)
+            if _max in finder and finder[_max] > 0: # 딕셔너리에 있고 값이 유효하다면 1개 줄이기
+                finder[_max] -= 1
                 break
+
     elif val == "-1": # 최소값 제거
         while minq:
             _min = heapq.heappop(minq)
-            if _min in finder:
-                finder.remove(_min)
+            if _min in finder and finder[_min] > 0:
+                finder[_min] -= 1
                 break
-def getMin():
-    while minq:
-        _min = minq[0]
-        if _min in finder:
-            return _min
-        heapq.heappop(minq)
-    return None
-
-def getMax():
-    while maxq:
-        _max = -maxq[0]
-        if _max in finder:
-            return _max
-        heapq.heappop(maxq)
-    return None
 
 tc = int(input())
 result = []
 for _ in range(tc):
     maxq = []
     minq = []
-    finder = set()
+    finder = {}
     opNum = int(input())
     for _ in range(opNum):
         op, val = input().rstrip().split()
@@ -50,10 +39,11 @@ for _ in range(tc):
             _insert(int(val))
         elif op == "D":
             _delete(val)
-    if not finder:
+    ret = [key for key, value in finder.items() if value != 0]
+    if not ret:
         result.append("EMPTY")
     else:
-        result.append(f"{getMax()} {getMin()}")
+        result.append(f"{max(ret)} {min(ret)}")
 
 for r in result:
     print(r)
