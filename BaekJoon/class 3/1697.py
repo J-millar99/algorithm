ef288418c53b6da7130c8891c432dc7d3a71c293
J-millar99 <wlwogus33@gmail.com>
2024-11-04 17:43:24 +0900

    bfs 메모니제이션

--- BaekJoon/class 3/1697.py
+++ BaekJoon/class 3/1697.py
@@ -1,20 +1,24 @@
-# 숨바꼭질
 from collections import deque
 n, k = map(int, input().split())
 memo = set()
 def bfs(start, time):
     q = deque([(start, time)])
     ret = 0 # 찾은 시간
+    memo.add(start)
     while q:
         v, t = q.popleft()
-        memo.add((v, t))
         ret = t
         if v == k:
             return ret
-        if (v, t) not in memo:
+        if v * 2 < 100001 and v * 2 not in memo:
             q.append((v * 2, t + 1))
+            memo.add(v * 2)
+        if v + 1 < 100001 and v + 1 not in memo:
             q.append((v + 1, t + 1))
+            memo.add(v + 1)
+        if v - 1 >= 0 and v - 1 not in memo:
             q.append((v - 1, t + 1))
-    return ret
+            memo.add(v - 1)
+
 print(bfs(n, 0))
             
