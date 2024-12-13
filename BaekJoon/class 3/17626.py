83677196454a9dae4faa0cce58e3d5f2b3280485
J-millar99 <wlwogus33@gmail.com>
2024-12-13 21:08:53 +0900

    dp

--- BaekJoon/class 3/17626.py
+++ BaekJoon/class 3/17626.py
@@ -1,13 +1,24 @@
-# Four Squares
-import math
+# Four Square
+import sys, math
+input = sys.stdin.readline
+def solution(n):
+    sqrt_num = int(math.sqrt(n))
+    # 제곱수인 경우 1 반환
+    if pow(sqrt_num, 2) == n:
+        return 1
+    # 두 제곱수의 합으로 표현되는 경우 체크
+    for i in range(1, sqrt_num + 1):
+        square_i = pow(i, 2)
+        if int(math.sqrt(n - square_i)) ** 2 == n - square_i:
+            return 2
+    # 세 제곱수의 합으로 표현되는 경우 체크
+    for i in range(1, sqrt_num + 1):
+        square_i = pow(i, 2)
+        for j in range(1, int(math.sqrt(n - square_i)) + 1):
+            square_j = pow(j, 2)
+            if int(math.sqrt(n - square_i - square_j)) ** 2 == n - square_i - square_j:
+                return 3
+    # 나머지는 모두 4
+    return 4
 n = int(input())
-cnt = 0
-
-while n > 0:
-    ret = int(math.sqrt(n))
-    print(ret)
-    ret *= ret
-    n -= ret
-    cnt += 1
-
-print(cnt)
+print(solution(n))
