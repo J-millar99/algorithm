5dc9e943b8bc02ddcdb6a1a51d4b9b70896e34ad
J-millar99 <wlwogus33@gmail.com>
2025-02-13 16:27:12 +0900

    자료구조 메모리 최적화

--- BaekJoon/classification/시뮬레이션/1966/Main.java
+++ BaekJoon/classification/시뮬레이션/1966/Main.java
@@ -4,8 +4,6 @@ import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Queue;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -20,27 +18,24 @@ public class Main {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int n = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
-            String[] strs = br.readLine().split(" ");
 
-            Map<Integer, Integer> weight = new HashMap<>();
-            for (int index = 0; index < strs.length; index++)
-                weight.put(index, Integer.parseInt(strs[index]));
-            printQueue(n, m, weight);
+            String[] strs = br.readLine().split(" ");
+            Queue<Integer> queue = new ArrayDeque<>();
+            List<Integer> priority = new ArrayList<>();
+
+            for (int j = 0; j < n; j++) {
+                Integer value = Integer.parseInt(strs[j]);
+                priority.add(value);
+                queue.add(value);
+            }
+
+            Collections.sort(priority, Collections.reverseOrder());
+            printQueue(queue, priority, m);
         }
         br.close();
     }
 
-    public static void printQueue(int size, int find, Map<Integer, Integer> weight) {
-        Queue<Integer> queue = new ArrayDeque<>();
-        List<Integer> priority = new ArrayList<>();
-    
-        for (int i = 0; i < size; i++) {
-            Integer value = weight.get(i);
-            priority.add(value);
-            queue.add(value);
-            }
-        Collections.sort(priority, Collections.reverseOrder());
-    
+    public static void printQueue(Queue<Integer> queue, List<Integer> priority, int find) {
         int count = 0;
         while (!queue.isEmpty()) {
             Integer request = queue.poll();
