472f086a93f8c4c7d498c21bd7108a3bea0c926c
J-millar99 <wlwogus33@gmail.com>
2025-03-08 14:29:16 +0900

    form fix

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
+
+        System.out.print(sb);
         br.close();
     }
 }
