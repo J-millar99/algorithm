8717ed1740a4db18459da8a2bbe457e10d226c1a
J-millar99 <wlwogus33@gmail.com>
2025-03-13 01:03:38 +0900

    다이나믹프로그래밍밍

---
+++ BaekJoon/Java/다이나믹프로그래밍/15988/Main.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        int tries = Integer.parseInt(br.readLine());
+        for (int i = 0; i < tries; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num > 2)
+                solution(num);
+            else
+                sb.append(num).append("\n");
+        }
+        System.out.print(sb);
+        br.close();
+    }
+
+    public static void solution(int num) {
+        long[] dp = new long[num + 1];
+        dp[1] = 1; // 1
+        dp[2] = 2; // 1 + 1, 2
+        dp[3] = 4; // 1 + 1 + 1, 1 + 2, 2 + 1, 3;
+        for (int i = 4; i <= num; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1000000009;
+        }
+        sb.append(dp[num]).append("\n");
+    }
+}
