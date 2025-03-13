5a1fb510c0d08a587a4d5eb2d77dbae00845beda
J-millar99 <wlwogus33@gmail.com>
2025-03-13 21:40:32 +0900

    다이나믹프로그래밍 - 퇴사 풀이중

--- BaekJoon/Java/다이나믹프로그래밍/14501/Main.java
+++ BaekJoon/Java/다이나믹프로그래밍/14501/Main.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.ArrayList;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,24 +13,30 @@ public class Main {
             this.price = price;
         }
     }
-    public static void main(String[] args) throws IOException {
-        int n = Integer.parseInt(br.readLine());
-        Date[] date = new Date[n + 1];
-        for (int i = 0; i < n; i++) {
-            String[] in = br.readLine().split(" ");
-            date[i + 1].day = Integer.parseInt(in[0]);
-            date[i + 1].price = Integer.parseInt(in[1]);
-        }
 
-        solution(1, n);
-        arr = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(br.readLine());
+        date = new Date[n + 1];
+        for (int i = 1; i <= n; i++) {
+            String[] in = br.readLine().split(" ");
+            int day = Integer.parseInt(in[0]);
+            int price = Integer.parseInt(in[1]);
+            date[i] = new Date(day, price);
+        }
+        solution();
         System.out.print(sb);
         br.close();
     }
     
-    public static void solution(int start, int day) {
-        for (int i = start; i <= n; i++) {
+    public static void solution() {
+        int[] dp = new int[n + 1];
+        int leave = n;
 
+        for (int i = 1; i <= n; i++) {
+            int cost = leave - date[i].day;
+            if (cost >= 0)
+                System.out.println("cost: " + cost);
+            leave--;
         }
     }
 }
