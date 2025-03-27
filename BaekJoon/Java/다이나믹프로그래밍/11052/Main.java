0ebd3599cc99d11b10bf716a7393da387245c1dd
J-millar99 <wlwogus33@gmail.com>
2025-03-28 00:42:00 +0900

    다이나믹프로그래밍

--- BaekJoon/Java/다이나믹프로그래밍/11052/Main.java
+++ BaekJoon/Java/다이나믹프로그래밍/11052/Main.java
@@ -10,16 +10,35 @@ public class Main {
         for (int i = 1; i <= num; i++) {
             cards[i] = Integer.parseInt(in[i - 1]);
         }
-        solution(num, cards);
+        System.out.println(solution(num, cards));
         br.close();
     }
 
-    public static void solution(int num, int[] cards) {
+    public static int solution(int num, int[] cards) {
         int[] dp = new int[num + 1];
+
         dp[1] = cards[1];
-        for (int i = 2; i <= num; i++) {
-            dp[i] = cards[i];
-            dp[i] = Math.max(dp[i], dp[i / 2] * 2);
+        if (num == 1)
+            return cards[1];
+
+        
+        dp[2] = Math.max(cards[2], dp[1] * 2);
+        if (num == 2)
+            return dp[num];
+
+
+        for (int i = 3; i <= num; i++)
+            dp[i] = Math.max(cards[i], combination(dp, i));
+
+        return dp[num];
+    }
+
+    public static int combination(int[] dp, int end) {
+        int result = 0;
+        for (int i = 1; i <= end / 2; i++) {
+            int sum = dp[i] + dp[end - i];
+            result = Math.max(result, sum);
         }
+        return result;
     }
 }
