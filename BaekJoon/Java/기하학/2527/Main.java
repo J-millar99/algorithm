b2c40d924a04d5aa66d17103dfce64eb3bc4d042
J-millar99 <wlwogus33@gmail.com>
2025-04-09 14:07:10 +0900

    기하학 - 푸는중

--- BaekJoon/Java/기하학/2527/Main.java
+++ BaekJoon/Java/기하학/2527/Main.java
@@ -33,24 +33,30 @@ public class Main {
     }
 
     public static boolean typeA(int[] square1, int[] square2) {
-        // 1. 직사각형이 포함관계거나 엮인 모양으로 직사각형을 만드는 경우
+        // 1. 오른쪽 위 겹침
         if (square1[0] <= square2[0] && square1[1] <= square2[1] &&
             square2[0] < square1[2] && square2[1] < square1[3])
             return true;
-
-        // 2. 1의 다른 직사각형이 기준
-        if (square2[0] <= square1[0] && square2[1] <= square1[1] &&
+        // 2. 왼쪽 아래 겹침
+        if (square1[2] >= square2[2] && square1[3] >= square2[3] &&
             square1[0] < square2[2] && square1[1] < square2[3])
             return true;
 
-        // 3. 직사각형이 관통하여 직사각형을 만드는 경우
-        if (square1[0] <= square2[0] && square2[0] < square1[2] &&
-            square2[1] < square1[3] && square1[3] <= square2[3])
+        // 3. 오른쪽 아래 겹침
+        if (square1[0] <= square2[0] && square1[3] >= square2[3] &&
+            square1[2] > square2[0] && square1[1] < square2[3])
             return true;
 
-        // 3. 3의 다른 직사각형이 기준
-        if (square2[0] <= square1[0] && square1[0] < square2[2] &&
-            square1[1] < square2[3] && square2[3] <= square1[3])
+        // 4. 왼쪽 위 겹침
+        if (square1[0] < square2[2] && square2[1] < square1[3] &&
+            square1[2] >= square2[2] && square1[1] <= square2[1])
+            return true;
+
+        if (square1[0] <= square2[0] && square2[0] < square1[2] &&
+            square2[1] <= square1[1] && square2[3] >= square1[3])
+            return true;
+        if (square1[0] < square2[0] && square2[0] <= square1[2] &&
+            square2[1] <= square1[1] && square2[3] >= square1[3])
             return true;
         return false;
     }
@@ -58,33 +64,29 @@ public class Main {
     public static boolean typeB(int[] square1, int[] square2) {
         // 1. 아래에서 접함
         if (square1[1] == square2[3]) {
-            if (square1[0] <= square2[0] && square2[0] < square1[2])
-                return true;
-            if (square1[0] < square2[2] && square2[2] <= square1[2])
+            if (square1[0] <= square2[0] && square2[0] < square1[2]
+                || square1[0] < square2[2] && square2[2] <= square1[2])
                 return true;
         }
         
         // 2. 위에서 접함
         if (square1[3] == square2[1]) {
-            if (square1[0] <= square2[0] && square2[0] < square1[2])
-                return true;
-            if (square1[0] < square2[2] && square2[2] <= square1[2])
+            if (square1[0] <= square2[0] && square2[0] < square1[2]
+                || square1[0] < square2[2] && square2[2] <= square1[2])
                 return true;
         }
 
         // 3. 왼쪽에서 접함
         if (square1[0] == square2[2]) {
-            if (square1[1] <= square2[1] && square2[1] < square1[3])
-                return true;
-            if (square1[1] < square2[3] && square2[3] <= square1[3])
+            if (square1[1] <= square2[1] && square2[1] < square1[3]
+                || square1[1] < square2[3] && square2[3] <= square1[3])
                 return true;
         }
 
         // 4. 오른쪽에서 접함
         if (square1[2] == square2[0]) {
-            if (square1[1] <= square2[1] && square2[1] < square1[3])
-                return true;
-            if (square1[1] < square2[3] && square2[3] <= square1[3])
+            if (square1[1] <= square2[1] && square2[1] < square1[3]
+                || square1[1] < square2[3] && square2[3] <= square1[3])
                 return true;
         }
         return false;
@@ -92,7 +94,7 @@ public class Main {
 
     public static boolean typeC(int[] square1, int[] square2) {
         // 1. 왼쪽 아래
-        if (square1[0] == square2[2] && square1[1] == square2[2])
+        if (square1[0] == square2[2] && square1[1] == square2[3])
             return true;
         
         // 2. 오른쪽 아래
@@ -109,16 +111,3 @@ public class Main {
         return false;        
     }
 }
-
-/*
-2 1 4 4 1 5 5 7
-2 1 5 4 1 4 1 8
-2 2 5 4 1 1 1 3
-2 2 4 5 1 1 2 1
-
-1 2 4 5 4 1 5 1
-1 2 4 4 5 1 5 7
-1 1 4 4 5 4 5 8
-1 1 4 4 4 5 8 8
- */
-
