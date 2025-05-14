81c074d5b77b0ba3aca1487e4a25db034d275b0c
J-millar99 <wlwogus33@gmail.com>
2025-05-14 13:43:40 +0900

    다이나믹 프로그래밍

--- BaekJoon/Java/다이나믹 프로그래밍/17404/Main.java
+++ BaekJoon/Java/다이나믹 프로그래밍/17404/Main.java
@@ -11,27 +11,37 @@ public class Main {
             field[i][2] = Integer.parseInt(in[1]);
             field[i][3] = Integer.parseInt(in[2]);
         }
-        System.out.println(solution(field, n));
+        solution(field, n);
         br.close();
     }
 
-    public static int solution(int[][] field, int n) {
-        int[][] dp = new int[n + 1][4];
-        
-        // init
-        dp[1][1] = field[1][1];
-        dp[1][2] = field[1][2];
-        dp[1][3] = field[1][3];
+    public static void solution(int[][] field, int n) {
+        final int INF = 1000000;
+        int answer = INF;
+        for (int firstColor = 1; firstColor <= 3; firstColor++) {
+            int[][] dp = new int[n + 1][4];
 
-        dp[2][1] = field[2][1] + Math.min(field[1][2], field[1][3]); 
-        dp[2][2] = field[2][2] + Math.min(field[1][1], field[1][3]);
-        dp[2][3] = field[2][3] + Math.min(field[1][1], field[1][2]);
+            // 첫 번째 색을 선택하고 나머지는 선택되지 않게 초기화
+            for (int color = 1; color <= 3; color++) {
+                if (color == firstColor)
+                    dp[1][color] = field[1][color];
+                else
+                    dp[1][color] = INF;
+            }
 
-        for (int i = 3; i <= n; i++) {
-            dp[i][1] = field[i][1] + Math.min(dp[i - 2][2] + field[i - 1][3], dp[i - 2][3] + field[i - 1][2]);
-            dp[i][2] = field[i][2] + Math.min(dp[i - 2][1] + field[i - 1][3], dp[i - 2][3] + field[i - 1][1]);
-            dp[i][3] = field[i][3] + Math.min(dp[i - 2][1] + field[i - 1][2], dp[i - 2][2] + field[i - 1][1]);
+            // System.out.println(dp[1][1] + " " + dp[1][2] + " " + dp[1][3]);
+            for (int i = 2; i <= n; i++) {
+                dp[i][1] = field[i][1] + Math.min(dp[i - 1][2], dp[i - 1][3]);
+                dp[i][2] = field[i][2] + Math.min(dp[i - 1][1], dp[i - 1][3]);
+                dp[i][3] = field[i][3] + Math.min(dp[i - 1][1], dp[i - 1][2]);
+                // System.out.println(dp[i][1] + " " + dp[i][2] + " " + dp[i][3]);
+            }
+
+            for (int lastColor = 1; lastColor <= 3; lastColor++) {
+                if (lastColor != firstColor)
+                    answer = Math.min(answer, dp[n][lastColor]);
+            }
         }
-        return Math.min(Math.min(dp[n][1], dp[n][2]), dp[n][3]);
+        System.out.println(answer);
     }
 }
