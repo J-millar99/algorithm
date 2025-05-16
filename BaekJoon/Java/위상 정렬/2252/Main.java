ad6547c1708365615ff79070c8f95732462e4579
J-millar99 <wlwogus33@gmail.com>
2025-05-16 13:50:24 +0900

    위상정렬 - 주석추가

--- BaekJoon/Java/위상 정렬/2252/Main.java
+++ BaekJoon/Java/위상 정렬/2252/Main.java
@@ -30,11 +30,13 @@ public class Main {
 
     public static void solution(List<List<Integer>> arr, int[] deg) {
         Queue<Integer> q = new ArrayDeque<>();
+        // q를 순회하며 deg가 0인 노드 번호만 넣는다
         for (int i = 1; i < arr.size(); i++) {
             if (deg[i] == 0)
                 q.add(i);
         }
 
+        // q에서 노드(deg == 0)를 뽑고 기록, 그 노드에 연결된 노드들의 deg를 줄이며 deg가 0이 되었을 때 큐에 넣는다
         while (!q.isEmpty()) {
             int node = q.poll();
             sb.append(node).append(" ");
