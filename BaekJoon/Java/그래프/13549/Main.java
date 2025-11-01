c846c8259a8ffabc30064b836ad0e59f7f09a975
J-millar99 <wlwogus33@gmail.com>
2025-11-01 22:43:52 +0900

    그래프 - 너비우선탐색

--- BaekJoon/Java/그래프/13549/Main.java
+++ BaekJoon/Java/그래프/13549/Main.java
@@ -7,36 +7,35 @@ public class Main {
     public static void main(String[] args) throws IOException {
         String[] in = br.readLine().split(" ");
         int n, k;
-        int count = 0;
+        int minTime = Integer.MAX_VALUE;
+        int found = Integer.MAX_VALUE;
 
         n = Integer.parseInt(in[0]); // 수빈이의 위치
         k = Integer.parseInt(in[1]); // 동생의 위치
 
         Queue<int[]> q = new ArrayDeque<>();
-        boolean visit[] = new boolean[LIMIT];
         q.add(new int[]{n, 0}); // 위치와 시간을 같이 저장
         while (true) {
             int[] pair = q.poll();
             int l = pair[0]; // 현재 위치
             int t = pair[1]; // 현재 시간
-            if (visit[l] == true)
-                continue;
-            if (l == k && visit[l] == false) { // 현재 위치가 동생의 위치라면 탐색 종료
-                count = t;
+            if (found < t)
                 break;
+            if (l == k) { // 현재 위치가 동생의 위치라면 같은 시간대의 큐를 탐색
+                minTime = Math.min(t, minTime);
+                found = t;
             }
-            visit[l] = true;
             int teleport = l * 2;
             int go = l + 1;
             int back = l - 1;
-            if (teleport < LIMIT && visit[teleport] == false)
+            if (teleport < LIMIT)
                 q.add(new int[]{teleport,  t});
-            if (go < LIMIT && visit[go] == false)
+            if (go < LIMIT)
                 q.add(new int[]{go,  t + 1});
-            if (l != 0 && visit[back] == false)
+            if (l != 0)
                 q.add(new int[]{back,  t + 1});
         }
 
-        System.out.println(count);
+        System.out.println(minTime);
     }
 }
