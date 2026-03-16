67c6bb4f004e1ef55f42ef9441e8311a6c1d7276
J-millar99 <wlwogus33@gmail.com>
2026-03-17 00:42:51 +0900

    이분탐색

--- BaekJoon/Java/수학/1072/Main.java
+++ BaekJoon/Java/수학/1072/Main.java
@@ -1,41 +1,46 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] in = br.readLine().split(" ");
+
         int x = Integer.parseInt(in[0]);
         int y = Integer.parseInt(in[1]);
-        solution(x, y);
+
+        System.out.println(solution(x, y));
         br.close();
     }
 
-    public static void solution(int x, int y) {
-        int origin = getRate(x, y);
-        int s = 0;
-        int e = x;
-        int result = 0;
-        while (s <= e) {
-            int m = (s + e) / 2;
-            int z = getRate(x + m, y + m);
-            if (origin == z) {
-                result = m;
-                s = m + 1;
-            } else
-                e = m - 1;
-        }
-        if (result == x)
-            System.out.println(-1);
-        else
-            System.out.println(result + 1);
-    }
-
-    public static int getRate(int x, int y) {
+    public static int getWinRate(int match, int win) {
         // 소수점 3자리 이하 손실, 그러나 정수 연산만 하므로 정확도 보장
-        int result = (int) ((long) y * 100 / x);
+        int result = (int) ((long) win * 100 / match);
         // 소수점을 손실하지 않지만 정확도에 오차가 존재
         // int result = (int) (((double) y / (double) x) * 100);
         return result;
+    }
+
+    public static int solution(int x, int y) {
+        int winRate = getWinRate(x, y);
+        int s = 0;
+        int e = x;
+
+        int result = 0;
+        while (s <= e) {
+            int m = (s + e) / 2;
+            int newRate = getWinRate(x + m, y + m);
+            if (winRate == newRate) { // 승 수를 더 올려야 함
+                s = m + 1;
+                result = m;
+            }
+            else { // 승 수를 덜 해도 됨
+                e = m - 1;
+            }
+        }
+        if (result == x)
+            return -1;
+        return result + 1;
     }
 }
