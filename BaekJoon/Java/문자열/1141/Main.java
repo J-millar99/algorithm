f0a3948d64f8fb5fe057bfffc757b29ddee0d4d0
J-millar99 <wlwogus33@gmail.com>
2026-03-20 17:54:41 +0900

    문자열

--- BaekJoon/Java/문자열/1141/Main.java
+++ BaekJoon/Java/문자열/1141/Main.java
@@ -2,33 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
-        int num = Integer.parseInt(br.readLine());
-        String[] words = new String[num];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        for (int i = 0; i < num; i++)
-            words[i] = br.readLine();
-        
-        // 오름차순 정렬 사용 (원래 코드)
-        Arrays.sort(words, Comparator.comparing(String::length));
+        int n = Integer.parseInt(br.readLine());
 
-        boolean[] isPrefix = new boolean[num];
-        for (int i = 0; i < num - 1; i++) {
-            for (int j = i + 1; j < num; j++) {
-                if (words[j].startsWith(words[i])) {
-                    isPrefix[i] = true;
+        ArrayList<String> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            arr.add(str);
+        }
+
+        arr.sort(Comparator.comparing(String::length));
+        boolean[] visit = new boolean[n];
+
+        for (int i = 0; i < n - 1; i++) {
+            String str1 = arr.get(i);
+            for (int j = i + 1; j < n; j++) {
+                String str2 = arr.get(j);
+                if (str2.startsWith(str1)) {
+                    visit[i] = true;
                     break;
                 }
             }
         }
-
-        int count = 0;
-        for (int i = 0; i < num; i++) {
-            if (!isPrefix[i]) count++;
-        }
-        
-        System.out.println(count);
         br.close();
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            if (visit[i] == false)
+                answer++;
+        }
+        System.out.println(answer);
     }
 }
