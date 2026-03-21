2a0e91fbc80460e6bdbcd5a36a52504c24abbf76
J-millar99 <wlwogus33@gmail.com>
2026-03-21 15:41:51 +0900

    다이나믹프로그래밍

--- BaekJoon/Java/다이나믹 프로그래밍/1149/Main.java
+++ BaekJoon/Java/다이나믹 프로그래밍/1149/Main.java
@@ -1,44 +1,38 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[][] graph = new int[n + 1][4];
 
+        int[][] field = new int[n][3];
         for (int i = 0; i < n; i++) {
             String[] in = br.readLine().split(" ");
-            for (int j = 0; j < 3; j++) {
-                graph[i + 1][j + 1] = Integer.parseInt(in[j]);
-            }
+            field[i][0] = Integer.parseInt(in[0]);
+            field[i][1] = Integer.parseInt(in[1]);
+            field[i][2] = Integer.parseInt(in[2]);
         }
 
-        solution(n, graph);
+        int[][] dp = new int[n][3];
+        dp[0][0] = field[0][0]; dp[0][1] = field[0][1]; dp[0][2] = field[0][2];
+
+        dp[1][0] = field[1][0] + Math.min(dp[0][1], dp[0][2]);
+        dp[1][1] = field[1][1] + Math.min(dp[0][0], dp[0][2]);
+        dp[1][2] = field[1][2] + Math.min(dp[0][0], dp[0][1]);
+ 
+        // 처음과 마지막 row는 합산 계산이 다르게 반영
+        for (int i = 2; i < n; i++) {
+            dp[i][0] = field[i][0] + Math.min(dp[i - 1][1], dp[i - 1][2]);
+            dp[i][1] = field[i][1] + Math.min(dp[i - 1][0], dp[i - 1][2]);
+            dp[i][2] = field[i][2] + Math.min(dp[i - 1][0], dp[i - 1][1]);
+        }
+
+        int answer = Integer.MAX_VALUE;
+        for (int i = 0; i < 3; i++) {
+            answer = Math.min(answer, dp[n - 1][i]);
+        }
         br.close();
-    }
-
-    public static void solution(int n, int[][] graph) {
-        int[][] dp = new int[n + 1][4];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= 3; j++) {
-                if (j == 1)
-                    dp[i][j] = graph[i][j] + Math.min(dp[i - 1][2], dp[i - 1][3]);
-                else if (j == 2)
-                    dp[i][j] = graph[i][j] + Math.min(dp[i - 1][1], dp[i - 1][3]);
-                else if (j == 3)
-                    dp[i][j] = graph[i][j] + Math.min(dp[i - 1][1], dp[i - 1][2]);
-            }
-        }
-        // for (int i = 1; i <= n; i++) {
-        //     for (int j = 1; j <= n; j++) {
-        //         System.out.print(dp[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
-        int result = dp[n][1];
-        for (int i = 2; i <= 3; i++) {
-            result = Math.min(result, dp[n][i]);
-        }
-        System.out.println(result);
+        System.out.println(answer);
     }
 }
